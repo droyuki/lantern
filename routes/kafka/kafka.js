083339b977d0b7f741e4539ff663ed9fb17a4406
Wilson Liao <droyuki@gmail.com>
2016-01-25 00:22:04 +0800

    v1.0, can send to kafka topic.

--- routes/kafka/kafka.js
+++ routes/kafka/kafka.js
@@ -1,13 +1,37 @@
 /**
  * Created by WeiChen on 2016/1/22.
  */
-var url  = require('url');
+var url = require('url');
 var Router = require('koa-router');
 var router = module.exports = new Router();
+var kafka = require('kafka-node');
 router.post('/kafka', function *() {
-    console.log("Kafka connect")
-    var msg = this.request.body
-    console.log(msg)
+    console.log("Kafka connect");
+    console.log(this.request.body);
+    console.log("to Str");
+    var msg = JSON.stringify(this.request.body, undefined, 1);
+    console.log(msg);
+    var Producer = kafka.Producer,
+        client = new kafka.Client('zookeeper1:2181,zookeeper2:2181,zookeeper3:2181'),
+        producer = new Producer(client);
+
+    console.log("create producer ok.")
+    var topic = "dispatch";
+    producer.on('ready', function () {
+        var message = 'a message';
+        //var keyedMessage = new KeyedMessage('keyed', 'a keyed message');
+
+        producer.send([
+            { topic: topic, partition: 0, messages: [msg], attributes: 0 }
+        ], function (err, result) {
+            console.log(err || result);
+        });
+    });
+
+    producer.on('error', function (err) {
+        console.log('error', err)
+    });
+
     this.body = {
         success: true
     };
